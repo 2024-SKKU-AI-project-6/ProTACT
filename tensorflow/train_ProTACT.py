a3d1f7ba21739bc26771a7efbc6da81ccaf7ebd1
ourgye <102244584+ourgye@users.noreply.github.com>
2024-05-15 23:50:10 +0900

    commit tensorflow

--- tensorflow/train_ProTACT.py
+++ tensorflow/train_ProTACT.py
@@ -12,42 +12,52 @@ from evaluators.multitask_evaluator_all_attributes import Evaluator as AllAttEva
 from tensorflow import keras
 import matplotlib.pyplot as plt
 
+
 class CustomHistory(keras.callbacks.Callback):
     def init(self):
         self.train_loss = []
         self.val_loss = []
         self.train_acc = []
-        self.val_acc = []        
-        
+        self.val_acc = []
+
     def on_epoch_end(self, batch, logs={}):
         self.train_loss.append(logs.get('loss'))
         self.val_loss.append(logs.get('val_loss'))
         self.train_acc.append(logs.get('acc'))
         self.val_acc.append(logs.get('val_acc'))
 
+
 def main():
+    # Argument parser
     parser = argparse.ArgumentParser(description="ProTACT model")
-    parser.add_argument('--test_prompt_id', type=int, default=1, help='prompt id of test essay set')
+    parser.add_argument('--test_prompt_id', type=int,
+                        default=1, help='prompt id of test essay set')
     parser.add_argument('--seed', type=int, default=12, help='set random seed')
     parser.add_argument('--model_name', type=str,
                         choices=['ProTACT'],
                         help='name of model')
-    parser.add_argument('--num_heads', type=int, default=2, help='set the number of heads in Multihead Attention')
-    parser.add_argument('--features_path', type=str, default='data/hand_crafted_v3.csv')
+    parser.add_argument('--num_heads', type=int, default=2,
+                        help='set the number of heads in Multihead Attention')
+    parser.add_argument('--features_path', type=str,
+                        default='data/hand_crafted_v3.csv')
     args = parser.parse_args()
     test_prompt_id = args.test_prompt_id
     seed = args.seed
     num_heads = args.num_heads
     features_path = args.features_path + str(test_prompt_id) + '.csv'
 
+    # Set random seed
     np.random.seed(seed)
     tf.random.set_seed(seed)
     random.seed(seed)
+    # Set python hash seed
     os.environ['PYTHONHASHSEED'] = str(seed)
 
-    print("Test prompt id is {} of type {}".format(test_prompt_id, type(test_prompt_id)))
+    print("Test prompt id is {} of type {}".format(
+        test_prompt_id, type(test_prompt_id)))
     print("Seed: {}".format(seed))
 
+    # Set configurations
     configs = Configs()
 
     data_path = configs.DATA_PATH
@@ -61,7 +71,8 @@ def main():
     vocab_size = configs.VOCAB_SIZE
     epochs = configs.EPOCHS
     batch_size = configs.BATCH_SIZE
-    print("Numhead : ", num_heads, " | Features : ", features_path, " | Pos_emb : ", configs.EMBEDDING_DIM)
+    print("Numhead : ", num_heads, " | Features : ",
+          features_path, " | Pos_emb : ", configs.EMBEDDING_DIM)
 
     read_configs = {
         'train_path': train_path,
@@ -73,24 +84,30 @@ def main():
     }
     # read POS for prompts
     pos_vocab = read_pos_vocab(read_configs)
-    prompt_pos_data = read_prompts_pos(prompt_path, pos_vocab) # for prompt POS embedding 
+    prompt_pos_data = read_prompts_pos(
+        prompt_path, pos_vocab)  # for prompt POS embedding
 
-    # read words for prompts 
+    # read words for prompts
     word_vocab = read_word_vocab(read_configs)
-    prompt_data = read_prompts_we(prompt_path, word_vocab) # for prompt word embedding 
-    
+    # for prompt word embedding
+    prompt_data = read_prompts_we(prompt_path, word_vocab)
+
     # read essays and prompts
-    train_data, dev_data, test_data = read_essays_prompts(read_configs, prompt_data, prompt_pos_data, pos_vocab) 
+    train_data, dev_data, test_data = read_essays_prompts(
+        read_configs, prompt_data, prompt_pos_data, pos_vocab)
 
     if pretrained_embedding:
         embedd_dict, embedd_dim, _ = load_word_embedding_dict(embedding_path)
-        embedd_matrix = build_embedd_table(word_vocab, embedd_dict, embedd_dim, caseless=True)
+        embedd_matrix = build_embedd_table(
+            word_vocab, embedd_dict, embedd_dim, caseless=True)
         embed_table = [embedd_matrix]
     else:
         embed_table = None
 
-    max_sentlen = max(train_data['max_sentlen'], dev_data['max_sentlen'], test_data['max_sentlen'])
-    max_sentnum = max(train_data['max_sentnum'], dev_data['max_sentnum'], test_data['max_sentnum'])
+    max_sentlen = max(train_data['max_sentlen'],
+                      dev_data['max_sentlen'], test_data['max_sentlen'])
+    max_sentnum = max(train_data['max_sentnum'],
+                      dev_data['max_sentnum'], test_data['max_sentnum'])
     prompt_max_sentlen = prompt_data['max_sentlen']
     prompt_max_sentnum = prompt_data['max_sentnum']
 
@@ -99,33 +116,58 @@ def main():
     print('max prompt sent length: {}'.format(prompt_max_sentlen))
     print('max prompt sent num: {}'.format(prompt_max_sentnum))
 
-    train_data['y_scaled'] = get_scaled_down_scores(train_data['data_y'], train_data['prompt_ids'])
-    dev_data['y_scaled'] = get_scaled_down_scores(dev_data['data_y'], dev_data['prompt_ids'])
-    test_data['y_scaled'] = get_scaled_down_scores(test_data['data_y'], test_data['prompt_ids'])
+    # numpy data
+    # rescale the scores from -1 to 1
+    train_data['y_scaled'] = get_scaled_down_scores(
+        train_data['data_y'], train_data['prompt_ids'])
+    dev_data['y_scaled'] = get_scaled_down_scores(
+        dev_data['data_y'], dev_data['prompt_ids'])
+    test_data['y_scaled'] = get_scaled_down_scores(
+        test_data['data_y'], test_data['prompt_ids'])
 
-    X_train_pos = pad_hierarchical_text_sequences(train_data['pos_x'], max_sentnum, max_sentlen)
-    X_dev_pos = pad_hierarchical_text_sequences(dev_data['pos_x'], max_sentnum, max_sentlen)
-    X_test_pos = pad_hierarchical_text_sequences(test_data['pos_x'], max_sentnum, max_sentlen)
+    # shape; (num_samples, max_sentnum, max_sentlen)
+    X_train_pos = pad_hierarchical_text_sequences(
+        train_data['pos_x'], max_sentnum, max_sentlen)
+    X_dev_pos = pad_hierarchical_text_sequences(
+        dev_data['pos_x'], max_sentnum, max_sentlen)
+    X_test_pos = pad_hierarchical_text_sequences(
+        test_data['pos_x'], max_sentnum, max_sentlen)
 
-    X_train_pos = X_train_pos.reshape((X_train_pos.shape[0], X_train_pos.shape[1] * X_train_pos.shape[2]))
-    X_dev_pos = X_dev_pos.reshape((X_dev_pos.shape[0], X_dev_pos.shape[1] * X_dev_pos.shape[2]))
-    X_test_pos = X_test_pos.reshape((X_test_pos.shape[0], X_test_pos.shape[1] * X_test_pos.shape[2]))
+    # shape; (num_samples, max_sentnum*max_sentlen)
+    X_train_pos = X_train_pos.reshape(
+        (X_train_pos.shape[0], X_train_pos.shape[1] * X_train_pos.shape[2]))
+    X_dev_pos = X_dev_pos.reshape(
+        (X_dev_pos.shape[0], X_dev_pos.shape[1] * X_dev_pos.shape[2]))
+    X_test_pos = X_test_pos.reshape(
+        (X_test_pos.shape[0], X_test_pos.shape[1] * X_test_pos.shape[2]))
 
-    X_train_prompt = pad_hierarchical_text_sequences(train_data['prompt_words'], max_sentnum, max_sentlen)
-    X_dev_prompt = pad_hierarchical_text_sequences(dev_data['prompt_words'], max_sentnum, max_sentlen)
-    X_test_prompt = pad_hierarchical_text_sequences(test_data['prompt_words'], max_sentnum, max_sentlen)
+    X_train_prompt = pad_hierarchical_text_sequences(
+        train_data['prompt_words'], max_sentnum, max_sentlen)
+    X_dev_prompt = pad_hierarchical_text_sequences(
+        dev_data['prompt_words'], max_sentnum, max_sentlen)
+    X_test_prompt = pad_hierarchical_text_sequences(
+        test_data['prompt_words'], max_sentnum, max_sentlen)
 
-    X_train_prompt = X_train_prompt.reshape((X_train_prompt.shape[0], X_train_prompt.shape[1] * X_train_prompt.shape[2]))
-    X_dev_prompt = X_dev_prompt.reshape((X_dev_prompt.shape[0], X_dev_prompt.shape[1] * X_dev_prompt.shape[2]))
-    X_test_prompt = X_test_prompt.reshape((X_test_prompt.shape[0], X_test_prompt.shape[1] * X_test_prompt.shape[2]))
+    X_train_prompt = X_train_prompt.reshape(
+        (X_train_prompt.shape[0], X_train_prompt.shape[1] * X_train_prompt.shape[2]))
+    X_dev_prompt = X_dev_prompt.reshape(
+        (X_dev_prompt.shape[0], X_dev_prompt.shape[1] * X_dev_prompt.shape[2]))
+    X_test_prompt = X_test_prompt.reshape(
+        (X_test_prompt.shape[0], X_test_prompt.shape[1] * X_test_prompt.shape[2]))
 
-    X_train_prompt_pos = pad_hierarchical_text_sequences(train_data['prompt_pos'], max_sentnum, max_sentlen)
-    X_dev_prompt_pos = pad_hierarchical_text_sequences(dev_data['prompt_pos'], max_sentnum, max_sentlen)
-    X_test_prompt_pos = pad_hierarchical_text_sequences(test_data['prompt_pos'], max_sentnum, max_sentlen)
+    X_train_prompt_pos = pad_hierarchical_text_sequences(
+        train_data['prompt_pos'], max_sentnum, max_sentlen)
+    X_dev_prompt_pos = pad_hierarchical_text_sequences(
+        dev_data['prompt_pos'], max_sentnum, max_sentlen)
+    X_test_prompt_pos = pad_hierarchical_text_sequences(
+        test_data['prompt_pos'], max_sentnum, max_sentlen)
 
-    X_train_prompt_pos = X_train_prompt_pos.reshape((X_train_prompt_pos.shape[0], X_train_prompt_pos.shape[1] * X_train_prompt_pos.shape[2]))
-    X_dev_prompt_pos = X_dev_prompt_pos.reshape((X_dev_prompt_pos.shape[0], X_dev_prompt_pos.shape[1] * X_dev_prompt_pos.shape[2]))
-    X_test_prompt_pos = X_test_prompt_pos.reshape((X_test_prompt_pos.shape[0], X_test_prompt_pos.shape[1] * X_test_prompt_pos.shape[2]))
+    X_train_prompt_pos = X_train_prompt_pos.reshape(
+        (X_train_prompt_pos.shape[0], X_train_prompt_pos.shape[1] * X_train_prompt_pos.shape[2]))
+    X_dev_prompt_pos = X_dev_prompt_pos.reshape(
+        (X_dev_prompt_pos.shape[0], X_dev_prompt_pos.shape[1] * X_dev_prompt_pos.shape[2]))
+    X_test_prompt_pos = X_test_prompt_pos.reshape(
+        (X_test_prompt_pos.shape[0], X_test_prompt_pos.shape[1] * X_test_prompt_pos.shape[2]))
 
     X_train_linguistic_features = np.array(train_data['features_x'])
     X_dev_linguistic_features = np.array(dev_data['features_x'])
@@ -171,33 +213,39 @@ def main():
     print('Y_test: ', Y_test.shape)
     print('================================')
 
-    train_features_list = [X_train_pos, X_train_prompt, X_train_prompt_pos, X_train_linguistic_features, X_train_readability]
-    dev_features_list = [X_dev_pos, X_dev_prompt, X_dev_prompt_pos, X_dev_linguistic_features, X_dev_readability]
-    test_features_list = [X_test_pos, X_test_prompt, X_test_prompt_pos, X_test_linguistic_features, X_test_readability]
+    # model training
+    # to gpu device
+    train_features_list = [X_train_pos, X_train_prompt,
+                           X_train_prompt_pos, X_train_linguistic_features, X_train_readability]
+    dev_features_list = [X_dev_pos, X_dev_prompt, X_dev_prompt_pos,
+                         X_dev_linguistic_features, X_dev_readability]
+    test_features_list = [X_test_pos, X_test_prompt, X_test_prompt_pos,
+                          X_test_linguistic_features, X_test_readability]
 
-    model = build_ProTACT(len(pos_vocab), len(word_vocab), max_sentnum, max_sentlen, 
-                      X_train_readability.shape[1],
-                      X_train_linguistic_features.shape[1],
-                      configs, Y_train.shape[1], num_heads, embed_table)
+    model = build_ProTACT(len(pos_vocab), len(word_vocab), max_sentnum, max_sentlen,
+                          X_train_readability.shape[1],
+                          X_train_linguistic_features.shape[1],
+                          configs, Y_train.shape[1], num_heads, embed_table)
 
     evaluator = AllAttEvaluator(test_prompt_id, dev_data['prompt_ids'], test_data['prompt_ids'], dev_features_list,
                                 test_features_list, Y_dev, Y_test, seed)
 
     evaluator.evaluate(model, -1, print_info=True)
 
-    custom_hist = CustomHistory() 
-    custom_hist.init() 
-    
+    custom_hist = CustomHistory()
+    custom_hist.init()
+
     for ii in range(epochs):
         print('Epoch %s/%s' % (str(ii + 1), epochs))
         start_time = time.time()
         model.fit(
             train_features_list,
-            Y_train, batch_size=batch_size, epochs=1, verbose=0, shuffle=True, validation_data=(dev_features_list,Y_dev),callbacks=[custom_hist])
+            Y_train, batch_size=batch_size, epochs=1, verbose=0, shuffle=True, validation_data=(dev_features_list, Y_dev), callbacks=[custom_hist])
         tt_time = time.time() - start_time
         print("Training one epoch in %.3f s" % tt_time)
         evaluator.evaluate(model, ii + 1)
-        print("Train Loss: ", custom_hist.train_loss[-1], "|| Val Loss: ", custom_hist.val_loss[-1])
+        print("Train Loss: ",
+              custom_hist.train_loss[-1], "|| Val Loss: ", custom_hist.val_loss[-1])
 
     evaluator.print_final_info()
 
